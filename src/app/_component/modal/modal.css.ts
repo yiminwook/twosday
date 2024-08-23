a2949f9f05a4bd16c840faec4997f9ec8e5f57ff
yiminwook <grs0416@gmail.com>
2024-08-23 22:19:16 +0900

    #DEV#MOD - modal 최대 최소 너비 조정

--- src/app/_component/modal/modal.css.ts
+++ src/app/_component/modal/modal.css.ts
@@ -1,7 +1,16 @@
-import { fixedMaxSize, flexCenter, zIndex } from "@/style/var";
+import { fixedMaxSize, flexCenter, responsive, zIndex } from "@/style/var";
 import { globalStyle, style } from "@vanilla-extract/css";
 
-export const modalLayout = style([zIndex.modal, flexCenter, fixedMaxSize, {}]);
+export const modalLayout = style([
+  zIndex.modal,
+  flexCenter,
+  fixedMaxSize,
+  {
+    "::-webkit-scrollbar": {
+      width: 3, // 스크롤바 너비
+    },
+  },
+]);
 
 export const modalCenterContent = style({
   position: "relative",
@@ -9,12 +18,45 @@ export const modalCenterContent = style({
   padding: 14,
   backgroundColor: "#fff",
   boxShadow: "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px",
-  borderRadius: 3,
+  borderRadius: 10,
+
+  maxWidth: "calc(100% - 20px)",
+  maxHeight: "calc(100% - 20px)",
   minHeight: 200,
+
+  overflowY: "auto",
+
   display: "flex",
   flexDirection: "column",
   justifyContent: "space-between",
 });
+
+export const modalBottomContent = style([
+  {
+    position: "relative",
+    width: "100%",
+    padding: 14,
+    backgroundColor: "#fff",
+    boxShadow: "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px",
+    borderRadius: "10px 10px 0 0",
+
+    maxHeight: "calc(100% - 10px)",
+    minHeight: 150,
+
+    overflowY: "auto",
+
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+
+    marginTop: "auto", // 아래로 붙이기
+  },
+  responsive({
+    sm: {
+      width: "auto", // 태블릿 이상에서는 컨텐츠 너비에 맞춰서
+    },
+  }),
+]);
 
 export const modalHeader = style({
   marginBottom: 7,
@@ -28,6 +70,7 @@ export const modalBtnBox = style({
   display: "flex",
   flexDirection: "row-reverse",
   gap: 14,
+  marginTop: 20,
 });
 
 globalStyle(`${modalBtnBox} > button`, {
