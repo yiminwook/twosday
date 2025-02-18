b3ec117aabcf6aaafcf36ac2a32f5f9ee2391255
Minwook Yi <grs0416@gmail.com>
2025-02-18 23:14:18 +0900

    #DEV#NEW - close pgClient when error occurred

--- src/libraries/pg/index.ts
+++ src/libraries/pg/index.ts
@@ -15,15 +15,15 @@ export function withPgConnection<TArgs extends any[], TResult>(
   fn: PGService<TArgs, TResult>,
 ): (...args: TArgs) => Promise<TResult> {
   return async (...args: TArgs): Promise<TResult> => {
-    try {
-      const pgClient = new Client({
-        host: process.env.PG_HOST,
-        port: Number(process.env.PG_PORT),
-        user: process.env.PG_USER,
-        password: process.env.PG_PASSWORD,
-        database: process.env.PG_DATABASE,
-      });
+    const pgClient = new Client({
+      host: process.env.PG_HOST,
+      port: Number(process.env.PG_PORT),
+      user: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DATABASE,
+    });
 
+    try {
       await pgClient.connect();
 
       // `await`를 추가하여 비동기 함수의 에러를 catch 블록에서 잡을 수 있도록 함
@@ -33,7 +33,7 @@ export function withPgConnection<TArgs extends any[], TResult>(
       throw error;
     } finally {
       try {
-        // await pgClient.end();
+        await pgClient.end();
       } catch (closeErr) {
         console.error("Error closing PG connection:", closeErr);
       }
