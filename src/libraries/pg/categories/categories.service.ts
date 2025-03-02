7ee0c713ceb1bd04e2a2c055e1dacb430b1d01e8
Minwook Yi <grs0416@gmail.com>
2025-03-03 02:23:37 +0900

    #DEV#MOD - getCategories service

--- src/libraries/pg/categories/categories.service.ts
+++ src/libraries/pg/categories/categories.service.ts
@@ -5,10 +5,18 @@ import { TPostCategoryDto, TPutCategoryDto } from "./categories.dto";
 export const getCategories = withPgConnection(async (client) => {
   const sql = `
     -- 페이지네이션 없이 모든 카테고리를 조회
-    SELECT  T01."id"        AS id,  
-            T01."parentId"  AS parentId, 
-            T01."name"      AS name
+    SELECT  T01."id"          AS categoryId,  
+            T01."parentId"    AS parentId, 
+            T01."name"        AS categoryName,
+            T03."id"          AS postId,
+            T03."title"       AS postTitle,
     FROM "${CATEGORIES_TABLE}" T01
+    LEFT JOIN "${CATEGORIES_TABLE}" T02 
+      -- 자기 자신을 부모로 가질 수 없음. CONSTRAINT:chk_no_self_reference
+      ON T01."id" = T02."parentId"
+    LEFT JOIN "POSTS" T03 
+      ON T02."id" = T03."categoryId"
+    ORDER BY T01."id", T03."id";
   `;
 
   const result = await client.query<{ id: number; parent_id: number | null; name: string }>(sql);
